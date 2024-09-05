caaaa13329f19db97343028572576e30aa2b3a86
1stuzzy <132016132+1stuzzy@users.noreply.github.com>
2024-09-05 21:51:43 +0500

    Delete states.py

--- states.py
+++
@@ -1,7 +0,0 @@
-from aiogram.dispatcher import FSMContext
-from aiogram.dispatcher.filters.state import State, StatesGroup
-
-
-class Form(StatesGroup):
-    selecting_warehouses = State()
-    confirming_selection = State()
